9e45c359235ad4d0cec0b3a2663dc029eddd17da
Robin H. Johnson <robbat2@gentoo.org>
2007-12-15 03:40:50 -0800

    Make util.py pass pylint.

--- gitosis/util.py
+++ gitosis/util.py
@@ -1,17 +1,33 @@
+"""
+Some utility functions for Gitosis
+"""
 import errno
 import os
 from ConfigParser import NoSectionError, NoOptionError
 
-def mkdir(*a, **kw):
+def mkdir(newdir, mode=0777):
+    """
+        Like os.mkdir, but already existing directories do not raise an error.
+    """
     try:
-        os.mkdir(*a, **kw)
-    except OSError, e:
-        if e.errno == errno.EEXIST:
+        os.mkdir(newdir, mode)
+    except OSError, ex:
+        if ex.errno == errno.EEXIST:
             pass
         else:
             raise
 
 def getRepositoryDir(config):
+    """
+        Find the location of the Git repositories.
+
+        Tries: 
+        - ``gitosis.repositories`` configuration key (see note)
+        - ``~/repositories``
+
+        Note: If the configuration key is a relative path, it is appended onto
+        the homedir for the gitosis user.
+    """
     repositories = os.path.expanduser('~')
     try:
         path = config.get('gitosis', 'repositories')
@@ -22,6 +38,13 @@ def getRepositoryDir(config):
     return repositories
 
 def getGeneratedFilesDir(config):
+    """
+        Find the location for the generated Gitosis files.
+
+        Tries: 
+        - ``gitosis.generate-files-in`` configuration key
+        - ``~/gitosis``
+    """
     try:
         generated = config.get('gitosis', 'generate-files-in')
     except (NoSectionError, NoOptionError):
